96c3932e4241b57e594b9de9f02fc4c2d22e4bf7
DivineWingedDemon <kikodabeira@gmail.com>
2021-12-29 14:39:50 +0000

    cliente documentado

--- Segunda Fase/SGCR/SGCRDataLayer/Clientes/ClientesFacade.java
+++ Segunda Fase/SGCR/SGCRDataLayer/Clientes/ClientesFacade.java
@@ -9,22 +9,30 @@ public class ClientesFacade implements Serializable {
 	Map<String,FichaCliente> clientes;
 	private int idProxEquipamento;
 
-
+	/**
+	 * Construtor do ClientesFacade
+	 */
 	public ClientesFacade() {
 		this.clientes = new HashMap<>();
 		this.idProxEquipamento = 1;
 	}
 
+	/**
+	 * Construtor do ClientesFacade
+	 * @param clientes mapa que associa o identificador do cliente(chave) à sua FichaCliente(valor)
+	 * @param idProxEquipamento string que é o identificador do próximo equipamento a ser registado
+	 */
 	public ClientesFacade(Map<String, FichaCliente> clientes, int idProxEquipamento) {
 		this.clientes = clientes;
 		this.idProxEquipamento = idProxEquipamento;
 	}
 
 	/**
-	 * 
-	 * @param Nome
-	 * @param NIF
-	 * @param Email
+	 * cria um novo cliente
+	 * @param Nome string que é o nome do novo cliente
+	 * @param NIF string que é o identificador/NIF do novo cliente
+	 * @param Email string que é o email do novo cliente
+	 * @return um bool dependendo se o cliente foi criado com sucesso ou não
 	 */
 	public boolean criaFichaCliente(String Nome, String NIF, String Email) {
 
@@ -34,8 +42,8 @@ public class ClientesFacade implements Serializable {
 	}
 
 	/**
-	 * 
-	 * @param NIF
+	 * @param NIF string do identificador do cliente
+	 * @return FichaCliente associado ao identificador NIF
 	 */
 	public FichaCliente getFichaCliente(String NIF) {
 		FichaCliente c =  clientes.get(NIF);
@@ -43,13 +51,16 @@ public class ClientesFacade implements Serializable {
 		else return null;
 	}
 
+	/**
+	 * @return a lista com todos os FichaCliente
+	 */
 	public List<FichaCliente> getFichasCLiente(){
 		return clientes.values().stream().map(FichaCliente::clone).collect(Collectors.toList());
 	}
 
 	/**
-	 * 
-	 * @param Email
+	 * @param Email string do email associado à FichaCliente em questão
+	 * @return FichaCLiente associado ao Email, ou null se não existir
 	 */
 	public FichaCliente getFichaClientePorEmail(String Email) {
 
@@ -60,9 +71,9 @@ public class ClientesFacade implements Serializable {
 	}
 
 	/**
-	 * 
-	 * @param idEquip
-	 * @param NIF
+	 * adicona um identificador de um equipamento/serviço, à lista de equipamentos na FichaCliente
+	 * @param idEquip string do identificador do equipamento a ser adicionado
+	 * @param NIF string do identificador do cliente a quem vai ser adicionado
 	 */
 	public boolean addEquipCliente(String idEquip, String NIF) {
 		if(!clientes.containsKey(NIF)) return false;
@@ -70,6 +81,10 @@ public class ClientesFacade implements Serializable {
 		return true;
 	}
 
+	/**
+	 * devolve a string do identificador do próximo equipamento a ser registado e atualiza o mesmo
+	 * @return string do identificador do próximo equipamento a ser registado
+	 */
 	public String getIdProxEquip() {
 		String id = String.valueOf(idProxEquipamento);
 		idProxEquipamento++;
