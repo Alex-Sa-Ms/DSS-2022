94e1701d42f59ba131c779812b991ce836b7f526
DivineWingedDemon <kikodabeira@gmail.com>
2021-12-29 15:53:00 +0000

    funcionarios documentados

--- Segunda Fase/SGCR/SGCRDataLayer/Funcionarios/Funcionario.java
+++ Segunda Fase/SGCR/SGCRDataLayer/Funcionarios/Funcionario.java
@@ -7,21 +7,38 @@ public abstract class Funcionario implements Serializable {
 	private String id;
 	private String password;
 
+	/**
+	 * @return string que é o identificador do funcionário
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * modifica o identificador do funcionário
+	 * @param id string que é o novo identificador do funcionário
+	 */
 	protected void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return string que é a palavra-passe do funcionário
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * modifica a palavra-passe do funcionário
+	 * @param password string que é a nova palavra-passe do funcionário
+	 */
 	protected void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * @return um Funcionario clone daquele que envocou este método
+	 */
 	public abstract Funcionario clone();
 }
